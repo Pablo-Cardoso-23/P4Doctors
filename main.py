e242a81474c259693cf9aa8afcec0e0e9f7e2bb0
Pablo-Cardoso-23 <pablohenriquevickinn1@gmail.com>
2026-03-24 22:55:29 -0300

    Criação das pastas e prototipagem da tela de login e funcionamento.

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from ui.forms import tela_login
+
+st.title('P4 Doctors')
+st.write('Bem-vindo ao seu gerenciador de consultas! Como podemos te ajudar hoje @User?')
---
+++ src/ui/forms.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import time
+
+st.set_page_config(page_title="Faça o Login Para Acessar o P4 Doctors", layout="centered")
+
+if "logged_in" not in st.session_state:
+    st.session_state['logged_in'] = False
+if "usuario_autenticado" not in st.session_state:
+    st.session_state['usuario_autenticado'] = ""
+
+def tela_login():
+    st.title("P4 Doctors")
+    st.markdown("Olá, seja bem-vindo! Faça login para acessar o sistema e aproveitar nossas funcionalidades!")
+
+    with st.form("form_login"):
+        usuario = st.text_input("Usuário", placeholder="Digite seu e-mail")
+        senha = st.text_input("Senha", type="password", placeholder="Digite sua senha")
+        botao_login = st.form_submit_button("Entrar", width="stretch")
+
+        if botao_login:
+            if usuario == "admin" and senha == "12345":
+                st.success("Login efetuado")
+                time.sleep(2)
+                st.session_state['logged_in'] = True
+                st.session_state['usuario_autenticado'] = usuario
+                st.rerun()
+            else:
+                st.error("Usuário ou senha incorretos")
+
+tela_login()
